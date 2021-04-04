760e465aee43b7714a7d8caea6da65aa42431080
Yeonho Lee <tomatoyeonho@naver.com>
2021-04-04 23:08:32 +0900

    Little bit changes

--- content.js
+++ content.js
@@ -1,16 +1,20 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.text && (msg.text == "participants")) {
-        let items = document.getElementsByClassName("GvcuGe")
-        let participantList = items[items.length - 1];
-        if (participantList == null) {
-            sendResponse([]);
-        } else {
-            var i;
-            var result = [];
-            for (i = 0; i < participantList.children.length; i++) {
-                result.push(participantList.children[i].children[0].children[1].children[0].children[0].innerHTML)
+        if (window.location.href.toString().includes("meet.google.com")) {
+            let items = document.getElementsByClassName("GvcuGe")
+            let participantList = items[items.length - 1];
+            if (participantList == null) {
+                sendResponse(null);
+            } else {
+                var i;
+                var result = [];
+                for (i = 0; i < participantList.children.length; i++) {
+                    result.push(participantList.children[i].children[0].children[1].children[0].children[0].innerHTML)
+                }
+                sendResponse(result);
             }
-            sendResponse(result);
+        } else {
+            sendResponse(null);
         }
     }
 })
--- popup.js
+++ popup.js
@@ -5,41 +5,49 @@ function update() {
         var currTab = tabs[0];
         if (currTab) {
             chrome.tabs.sendMessage(currTab.id, {text: "participants"}, function(result) {
-                chrome.storage.local.get("list", ({ list }) => {
-                    set.innerHTML = '';
-                    let legend = document.createElement("span");
-                    legend.innerHTML = "&#9989; 출석 / &#10060; 결석<br>(0명 출석, 0명 결석)";
-                    let br = document.createElement("br");
-                    set.appendChild(legend);
-                    set.appendChild(br);
-                    var l = JSON.parse(list);
-                    var i;
-                    var att_count = 0;
-                    var abs_count = 0;
-                    for (i = 0; i < l.length; i++) {
-                        let parag = document.createElement("span");
-                        let emoji = document.createElement("span");
-                        emoji.style.fontFamily = "Symbola";
-                        if (result.includes(l[i])) {
-                            parag.innerText = l[i];
-                            parag.style.color = "green";
-                            emoji.innerHTML = " &#9989; ";
-                            att_count += 1;
-                        } else {
-                            parag.innerText = l[i];
-                            parag.style.color = "red";
-                            emoji.innerHTML = " &#10060; ";
-                            abs_count += 1;
+                set.innerHTML = '';
+                if (result == null) {
+                    let oops = document.createElement("span");
+                    oops.innerHTML = "참여자 정보를 가져올 수 없습니다.<br>";
+                    let info = document.createElement("span");
+                    info.innerHTML = "현재 열린 페이지가 Google Meet 페이지인지,<br>참여자 리스트가 열려 있는지 확인하십시오."
+                    set.appendChild(oops);
+                    set.appendChild(info);
+                } else {
+                    chrome.storage.local.get("list", ({ list }) => {
+                        let legend = document.createElement("span");
+                        legend.innerHTML = "&#9989; 출석 / &#10060; 결석<br>(0명 출석, 0명 결석)";
+                        let br = document.createElement("br");
+                        set.appendChild(legend);
+                        set.appendChild(br);
+                        var l = JSON.parse(list);
+                        var i;
+                        var att_count = 0;
+                        var abs_count = 0;
+                        for (i = 0; i < l.length; i++) {
+                            let parag = document.createElement("span");
+                            let emoji = document.createElement("span");
+                            if (result.includes(l[i])) {
+                                parag.innerText = l[i];
+                                parag.style.color = "green";
+                                emoji.innerHTML = " &#9989; ";
+                                att_count += 1;
+                            } else {
+                                parag.innerText = l[i];
+                                parag.style.color = "red";
+                                emoji.innerHTML = " &#10060; ";
+                                abs_count += 1;
+                            }
+                            set.appendChild(parag);
+                            set.appendChild(emoji);
+                            if ((i + 1) % 3 == 0) {
+                                let br = document.createElement("br");
+                                set.appendChild(br);
+                            }
                         }
-                        set.appendChild(parag);
-                        set.appendChild(emoji);
-                        if ((i + 1) % 3 == 0) {
-                            let br = document.createElement("br");
-                            set.appendChild(br);
-                        }
-                    }
-                    legend.innerHTML = "&#9989; 출석 / &#10060; 결석<br>(" + att_count + "명 출석, " + abs_count + "명 결석)";
-                })
+                        legend.innerHTML = "&#9989; 출석 / &#10060; 결석<br>(" + att_count + "명 출석, " + abs_count + "명 결석)";
+                    });
+                }
             });
         }
     });
