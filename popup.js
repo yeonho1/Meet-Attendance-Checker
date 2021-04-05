c451e552988acb74bed0075d3695540a7a7917e6
Yeonho Lee <tomatoyeonho@naver.com>
2021-04-05 09:01:02 +0900

    blinking issue fixed

--- popup.js
+++ popup.js
@@ -27,47 +27,36 @@ function update() {
         var currTab = tabs[0];
         if (currTab) {
             chrome.tabs.sendMessage(currTab.id, {text: "participants"}, function(result) {
-                set.innerHTML = '';
                 if (result == null || chrome.runtime.lastError) {
-                    let oops = document.createElement("span");
-                    oops.innerHTML = "참여자 정보를 가져올 수 없습니다.<br>";
-                    let info = document.createElement("span");
-                    info.innerHTML = "현재 열린 페이지가 Google Meet 페이지인지,<br>참여자 리스트가 열려 있는지 확인하십시오."
-                    set.appendChild(oops);
-                    set.appendChild(info);
+                    set.innerHTML = "참여자 정보를 가져올 수 없습니다.";
+                    set.innerHTML += "<br>현재 열린 페이지가 Google Meet 페이지인지,";
+                    set.innerHTML += "<br>참여자 리스트가 열려 있는지 확인하십시오.";
                 } else {
                     chrome.storage.local.get("list", ({ list }) => {
-                        let legend = document.createElement("span");
-                        legend.innerHTML = "&#9989; 출석 / &#10060; 결석<br>(0명 출석, 0명 결석)";
-                        let br = document.createElement("br");
-                        set.appendChild(legend);
-                        set.appendChild(br);
                         var l = JSON.parse(list);
                         var i;
                         var att_count = 0;
                         var abs_count = 0;
+                        let allString = "";
                         for (i = 0; i < l.length; i++) {
-                            let parag = document.createElement("span");
-                            let emoji = document.createElement("span");
+                            var emoji = " &#10060; ";
+                            let style = ""
                             if (result.includes(l[i])) {
-                                parag.innerText = l[i];
-                                parag.style.color = "green";
-                                emoji.innerHTML = " &#9989; ";
+                                emoji = " &#9989; ";
+                                style = "color: green;";
                                 att_count += 1;
                             } else {
-                                parag.innerText = l[i];
-                                parag.style.color = "red";
-                                emoji.innerHTML = " &#10060; ";
+                                style = "color: red;";
                                 abs_count += 1;
                             }
-                            set.appendChild(parag);
-                            set.appendChild(emoji);
+                            allString += "<span style='" + style + "'>" + l[i] + "</span>";
+                            allString += "<span>" + emoji + "</span>";
                             if ((i + 1) % 3 == 0) {
-                                let br = document.createElement("br");
-                                set.appendChild(br);
+                                allString += "<br>"
                             }
                         }
-                        legend.innerHTML = "&#9989; 출석 / &#10060; 결석<br>(" + att_count + "명 출석, " + abs_count + "명 결석)";
+                        allString = "&#9989; 출석 / &#10060; 결석<br>(" + att_count + "명 출석, " + abs_count + "명 결석)<br>" + allString;
+                        set.innerHTML = allString;
                     });
                 }
             });
