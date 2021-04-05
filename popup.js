5bb5e869845d398789891c8e4387fa69ad5b4591
Yeonho Lee <tomatoyeonho@naver.com>
2021-04-05 13:34:52 +0900

    disabling button when unavailable

--- popup.js
+++ popup.js
@@ -31,6 +31,7 @@ function update() {
                     set.innerHTML = "참여자 정보를 가져올 수 없습니다.";
                     set.innerHTML += "<br>현재 열린 페이지가 Google Meet 페이지인지,";
                     set.innerHTML += "<br>참여자 리스트가 열려 있는지 확인하십시오.";
+                    copyButton.disabled = true;
                 } else {
                     chrome.storage.local.get("list", ({ list }) => {
                         var l = JSON.parse(list);
@@ -57,6 +58,7 @@ function update() {
                         }
                         allString = "&#9989; 출석 / &#10060; 결석<br>(" + att_count + "명 출석, " + abs_count + "명 결석)<br>" + allString;
                         set.innerHTML = allString;
+                        copyButton.disabled = false;
                     });
                 }
             });
